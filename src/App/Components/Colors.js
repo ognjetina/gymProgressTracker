ec870d4c62a1004ea6ec3d0612868dad17211bbf
anitejngo <laogdo@gmail.com>
2020-01-29 00:15:52 +0100

    Add picker and start workout button to practice screen

--- src/App/Components/Colors.js
+++ src/App/Components/Colors.js
@@ -2,5 +2,6 @@ export const Colors = {
 	PRIMARY: '#002D64',
 	ACTIVE: '#BADA55',
 	INACTIVE: '#bfeaff',
-	BACKGROUND: '#80d6ff'
+	BACKGROUND: '#80d6ff',
+	WHITE:"#fff"
 };
--- src/App/Screens/Practice/PracticeScreen.js
+++ src/App/Screens/Practice/PracticeScreen.js
@@ -1,13 +1,51 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Picker, Text, TouchableOpacity } from 'react-native';
+import { Colors } from '../../Components';
 
 
 export const PracticeScreen = ({ navigation }) => {
 
+	const [workout, setWorkout] = useState();
+
+	const workouts = [
+		'Deadlift', 'Squat', 'Biceps curl', 'Bench press', 'Leg curl', 'Bent over row', 'Shoulder Press'
+	];
+
 	return (
 		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 
-			<Text>Practice screen</Text>
+			<View
+				style={{
+					height: 40,
+					width: 300,
+					backgroundColor: Colors.INACTIVE,
+					justifyContent: 'center',
+					borderRadius: 20,
+					paddingLeft: 10,
+					marginBottom: 80
+				}}>
+
+				<Picker selectedValue={workout} onValueChange={value => {
+					setWorkout(value)
+				}}>
+					{
+						workouts.map(workout => {
+							return <Picker.Item key={workout} label={workout} value={workout} color={Colors.PRIMARY} />
+						})
+					}
+				</Picker>
+
+			</View>
+			<TouchableOpacity style={{
+				height: 40,
+				width: 300,
+				backgroundColor: Colors.PRIMARY,
+				alignItems: 'center',
+				justifyContent: 'center',
+				borderRadius: 20,
+			}}>
+				<Text style={{ fontSize: 24, fontWeight: 'bold', color: Colors.WHITE }}>Start workout</Text>
+			</TouchableOpacity>
 		</View>
 	)
 }
