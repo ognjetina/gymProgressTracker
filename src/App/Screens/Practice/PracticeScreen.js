65652f4fa422793df18b83006b564c892c390269
anitejngo <laogdo@gmail.com>
2020-02-11 09:25:02 +0100

    Change practice screen to scroll list instead of dropDown

--- src/App/Screens/Practice/PracticeScreen.js
+++ src/App/Screens/Practice/PracticeScreen.js
@@ -1,51 +1,138 @@
-import React, { useState } from 'react';
-import { View, Picker, Text, TouchableOpacity } from 'react-native';
-import { Colors } from '../../Components';
+import React, {userRef} from 'react';
+import {View, ScrollView, Text, StyleSheet, Animated} from 'react-native';
+import {Colors} from '../../Components';
 
+export const PracticeScreen = ({navigation}) => {
+  const workouts = [
+    {
+      name: 'Deadlift',
+      iconName: 'arrow',
+      description:
+        'This is a nice description, what this workout is good for! ' +
+        'This is a nice description, what this workout is good for!',
+    },
+    {
+      name: 'Squat',
+      iconName: 'arrow',
+      description: 'This is a nice description, what this workout is good for!',
+    },
+    {
+      name: 'Biceps curl',
+      iconName: 'arrow',
+      description: 'This is a nice description, what this workout is good for!',
+    },
+    {
+      name: 'Bench press',
+      iconName: 'arrow',
+      description: 'This is a nice description, what this workout is good for!',
+    },
+    {
+      name: 'Leg curl',
+      iconName: 'arrow',
+      description: 'This is a nice description, what this workout is good for!',
+    },
+    {
+      name: 'Bent over row',
+      iconName: 'arrow',
+      description: 'This is a nice description, what this workout is good for!',
+    },
+    {
+      name: 'Shoulder Press',
+      iconName: 'arrow',
+      description: 'This is a nice description, what this workout is good for!',
+    },
+  ];
 
-export const PracticeScreen = ({ navigation }) => {
+  const scrollY = new Animated.Value(0);
 
-	const [workout, setWorkout] = useState();
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.titleText}>WORKOUTS</Text>
+      </View>
 
-	const workouts = [
-		'Deadlift', 'Squat', 'Biceps curl', 'Bench press', 'Leg curl', 'Bent over row', 'Shoulder Press'
-	];
+      <ScrollView
+        style={styles.scrollViewContainer}
+        onScroll={event => scrollY.setValue(event.nativeEvent.contentOffset.y)}>
+        {workouts.map(workout => {
+          return (
+            <View key={workout.name} style={styles.cardContainer}>
+              <View style={styles.cardBodyContainer}>
+                <View>
+                  <Text style={styles.cardTitle}>{workout.name}</Text>
+                </View>
+                <View style={styles.cardDescriptionContainer}>
+                  <Text style={styles.cardDescriptionText}>
+                    {workout.description}
+                  </Text>
+                </View>
+              </View>
+            </View>
+          );
+        })}
+      </ScrollView>
+    </View>
+  );
+};
 
-	return (
-		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-
-			<View
-				style={{
-					height: 40,
-					width: 300,
-					backgroundColor: Colors.INACTIVE,
-					justifyContent: 'center',
-					borderRadius: 20,
-					paddingLeft: 10,
-					marginBottom: 80
-				}}>
-
-				<Picker selectedValue={workout} onValueChange={value => {
-					setWorkout(value)
-				}}>
-					{
-						workouts.map(workout => {
-							return <Picker.Item key={workout} label={workout} value={workout} color={Colors.PRIMARY} />
-						})
-					}
-				</Picker>
-
-			</View>
-			<TouchableOpacity style={{
-				height: 40,
-				width: 300,
-				backgroundColor: Colors.PRIMARY,
-				alignItems: 'center',
-				justifyContent: 'center',
-				borderRadius: 20,
-			}}>
-				<Text style={{ fontSize: 24, fontWeight: 'bold', color: Colors.WHITE }}>Start workout</Text>
-			</TouchableOpacity>
-		</View>
-	)
-}
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 24,
+    alignSelf: 'stretch',
+  },
+  titleContainer: {
+    marginBottom: 8,
+    alignSelf: 'stretch',
+    alignItems: 'flex-start',
+    paddingHorizontal: 24,
+    justifyContent: 'flex-end',
+	  borderBottomWidth: 1
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: Colors.PRIMARY,
+  },
+  scrollViewContainer: {
+    flex: 1,
+    alignSelf: 'stretch',
+    paddingHorizontal: 32,
+    paddingTop: 26,
+    borderColor: Colors.PRIMARY,
+  },
+  cardContainer: {
+    flex: 1,
+    backgroundColor: Colors.INACTIVE,
+    height: 140,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 8,
+    borderRadius: 16,
+    padding: 16,
+  },
+  cardImage: {
+    width: 100,
+    height: '100%',
+    alignSelf: 'flex-start',
+  },
+  cardBodyContainer: {
+    flex: 3,
+    alignSelf: 'stretch',
+    paddingHorizontal: 14,
+  },
+  cardTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: Colors.PRIMARY,
+  },
+  cardDescriptionContainer: {
+    flex: 1,
+    paddingTop: 8,
+  },
+  cardDescriptionText: {
+    color: Colors.PRIMARY,
+  },
+});
